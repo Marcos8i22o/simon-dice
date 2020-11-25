080cdefa3ede62371782d6764815cdb98fd1679e
Marcos8i22o <marcos.jinno@gmail.com>
2020-11-25 10:43:11 -0300

    Agrega feature resaltar cuadro seleccionado y funciones del botón Empezar
    Cuando se elije un cuadro, se resalta.
    Las funciones del botón Empezar son 2:
    * Deshabilitar el botón cuando la máquina está jugando
    * Habilitar el botón...falta decidir cuándo llamar a ésta función.

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ let secuenciaUsuario = [];
 $botonEmpezar.onclick = function (event) {
     actualizarEstado("Espere su turno");
     //bloquearInputUsuario();
-
+    obtenerCuadroAleatorio().style.opacity = 1; //Resalta cuadro
+    deshabilitarBotonEmpezar();
     event.preventDefault();
 }
 
@@ -15,11 +16,18 @@ function obtenerCuadroAleatorio() {
     const $cuadros = document.querySelectorAll('.cuadro');
     const indice = Math.floor(Math.random() * $cuadros.length);
     return $cuadros[indice];
-
 }
+
 
 function actualizarEstado (estado) {
     document.querySelector('#estado').textContent = estado;
 }
 
+function deshabilitarBotonEmpezar () {
+    $botonEmpezar.disabled = true;
+}
+
+function habilitarBotonEmpezar () {
+    $botonEmpezar.disabled = false;
+}
 
