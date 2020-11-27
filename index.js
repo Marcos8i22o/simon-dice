5d8dc9e29f16bdc825605f069ba3b2221e3cb926
Marcos8i22o <marcos.jinno@gmail.com>
2020-11-27 12:53:10 -0300

    Agrega manejarInputUsuario
    Resalta el cuadro seleccionado por el usuario

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
 let $botonEmpezar = document.querySelector('#boton-empezar');
 
+let $botonEmpezar = document.querySelector("#boton-empezar");
+
 let secuenciaMaquina = [];
 let secuenciaUsuario = [];
 
 
 $botonEmpezar.onclick = function (event) {
-    actualizarEstado("Espere su turno");
-    //bloquearInputUsuario();
-    obtenerCuadroAleatorio().style.opacity = 1; //Resalta cuadro
-    deshabilitarBotonEmpezar();
-    event.preventDefault();
+  actualizarEstado("Espere su turno");
+  //bloquearInputUsuario();
+
+  secuenciaMaquina.push(obtenerCuadroAleatorio());
+  secuenciaMaquina[secuenciaMaquina.length - 1].style.opacity = 1;
+
+  deshabilitarBotonEmpezar();
+  event.preventDefault();
+};
+
+/*******************USUARIO*************************/
+function manejarInputUsuario(e) {
+  const indiceUsuario =  (e.target.id);  
+  const $cuadroUsuario = document.querySelector(`#${indiceUsuario}`);
+	opacidad(indiceUsuario,1);
 }
+
+document.querySelector('body').onclick = manejarInputUsuario;
+ 
+
+function opacidad(numero,grado) {
+  document.querySelector(`#${numero}`).style.opacity = grado;
+}
+/*****************FIN USUARIO************************** */
 
 function obtenerCuadroAleatorio() {
-    const $cuadros = document.querySelectorAll('.cuadro');
-    const indice = Math.floor(Math.random() * $cuadros.length);
-    return $cuadros[indice];
+  const $cuadros = document.querySelectorAll(".cuadro");
+  const indice = Math.floor(Math.random() * $cuadros.length);
+  return $cuadros[indice];
 }
 
-
-function actualizarEstado (estado) {
-    document.querySelector('#estado').textContent = estado;
+function actualizarEstado(estado) {
+  document.querySelector("#estado").textContent = estado;
 }
 
-function deshabilitarBotonEmpezar () {
-    $botonEmpezar.disabled = true;
+function deshabilitarBotonEmpezar() {
+  $botonEmpezar.disabled = true;
 }
 
-function habilitarBotonEmpezar () {
-    $botonEmpezar.disabled = false;
+function habilitarBotonEmpezar() {
+  $botonEmpezar.disabled = false;
 }
-
